35ba1899a8e321d490436a8321c6c0ed70d747ed
unknown <vutienlocd1nhvt@gmail.com>
2022-06-27 23:00:44 +0700

    first commit

---
+++ ProjectReactJS-main/src/redux/userSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from './store';
+import { userInfo } from './types';
+
+
+const initialState: userInfo = {
+
+  userName: "tienloc",
+  fullName: "tienloc",
+  phoneNumber: "0335449948",
+  email: "lokbugbug@gmail.com",
+  password: "123456",
+  role: 0,
+  avatar: "https://scontent.fsgn2-1.fna.fbcdn.net/v/t39.30808-6/251374287_946849012708805_4517610628801416846_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=UFFxfmnuFnYAX_zgTpx&_nc_ht=scontent.fsgn2-1.fna&oh=00_AT8YaTA4n0UKCWjbSztQJJ7dFmtXzRb43MlEsIRzugak3g&oe=62AF86BD"
+}
+
+  export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+      login: (state, actions) => {
+        state.email = actions.payload.email
+      },
+      logout: (state) => {
+        state.email = ""
+      }
+    },
+  });
+  
+  // To able to use reducers we need to export them.
+  export const { login, logout } = userSlice.actions;
+  
+  export default userSlice.reducer;
